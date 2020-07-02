a00a1443f2b648b5580b88cb82eace4e07cdf7c8
ait <ait@ex.in>
2020-07-03 05:51:08 +0900

    [Tax] tax calc

--- src/js/app2.js
+++ src/js/app2.js
@@ -1,7 +1,8 @@
-let money = parseInt(prompt("値段"))
+const money = parseInt(prompt("値段"))
+const tax = 10
 
 if (Number.isFinite(money)) {
-  document.write(`<p>${money}円</p>`);
+  document.write(`<p>${ Math.round(money + ((money * tax) / 100)) }円</p>`);
 } else {
   document.write('数字入れて');
 }
