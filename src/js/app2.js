00aaa1609e952d98cb5c1da98341c6a7eca16b05
ait <ait@ex.in>
2020-07-03 05:39:02 +0900

    [Prepros Set]

--- src/js/app2.js
+++ src/js/app2.js
@@ -3,5 +3,5 @@ let money = parseInt(prompt("値段"))
 if (Number.isFinite(money)) {
   document.write(`<p>${money}円</p>`);
 } else {
-  document.write('見つかりませんでした');
+  document.write('数字入れて');
 }
---
+++ src/js/main.js
@@ -0,0 +1,1 @@
+// @prepros-append  app2.js
